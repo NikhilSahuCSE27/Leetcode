9229f207240d29e70f066a8d67539147c9571050
Nikhil Sahu <sahunn@rknec.edu>
2026-05-29 01:42:21 +0530

    Time: 0 ms (100.00%) | Memory: 45.5 MB (26.61%) - LeetSync

---
+++ 330-patching-array/patching-array.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int minPatches(int[] nums,int n){
+    long miss=1;
+    int i=0,res=0;
+    while(miss<=n){
+        if(i<nums.length && nums[i]<=miss){
+            miss=miss+nums[i];
+            i++;
+        }else{
+            miss=miss+miss;
+            res++;
+        }
+    }
+    return res;
+}
+}
