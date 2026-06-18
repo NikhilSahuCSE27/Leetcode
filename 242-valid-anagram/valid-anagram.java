3fd5eb31bbd91f232b5c88792e5640c9fe11be5f
Nikhil Sahu <sahunn@rknec.edu>
2026-06-18 17:11:45 +0530

    Time: 4 ms (82.17%) | Memory: 46.6 MB (15.61%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -3,27 +3,15 @@ class Solution {
         if(s.length()!=t.length()){
             return false;
         }
-        boolean flag=true;
-       int arr1[]=new int[s.length()];
-       int arr2[]=new int[t.length()];
-      
-       for(int i=0;i<s.length();i++){
-        char ch=s.charAt(i);
-        arr1[i]=ch;
-       }
-       for(int i=0;i<t.length();i++){
-        char ch=t.charAt(i);
-        arr2[i]=ch;
-       }
-       Arrays.sort(arr1);
-       Arrays.sort(arr2);
-      for(int i=0;i<arr1.length;i++){
-        if(arr1[i]!=arr2[i]){
-            flag=false;
-            break;
+        char a[]=s.toCharArray();
+        char b[]=t.toCharArray();
+        Arrays.sort(a);
+        Arrays.sort(b);
+        for(int i=0;i<s.length();i++){
+            if(a[i]!=b[i]){
+                return false;
+            }
         }
-      }
-      return flag;
-
+        return true;
     }
 }
